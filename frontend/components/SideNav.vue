4e334557f99fd4edbbff0f1af438ddc0a0a515a6
Mikołaj <gawrmiko@gmail.com>
2026-04-29 22:15:38 +0200

    fix

--- frontend/components/SideNav.vue
+++ frontend/components/SideNav.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="SideNav" :class="route.fullPath === '/' ? 'lg:w-[310px]' : 'lg:w-[220px]'"
     class="fixed z-20 bg-white pt-[70px] h-full lg:border-r-0 border-r w-[75px] overflow-auto">
-    <div class="lg:w-full w-[55px] mx-auto">
+    
+    <div class="lg:w-full w-full pl-2 lg:pl-0">
       <NuxtLink to="/">
-        <MenuItem iconString="Dla Ciebie" colorString="#bc2cf0" sizeString="30" />
+        <MenuItem iconString="Dla Ciebie" colorString="#bc2cf0" sizeString="30" class="lg:ml-2" />
       </NuxtLink>
 
-      <div class="border-b lg:ml-2 mt-2" />
+      <div class="border-b lg:ml-2 mt-2 mr-2 lg:mr-0" />
 
       <div class="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">
         Sugerowane konta
@@ -16,18 +17,18 @@
 
       <div v-if="$generalStore.suggested" v-for="sug in $generalStore.suggested" :key="sug.id">
         <div @click="isLoggedIn(sug)" class="cursor-pointer">
-          <MenuItemFollow :user="sug" />
+          <MenuItemFollow :user="sug" class="lg:ml-2" />
         </div>
       </div>
 
-      <button class="lg:block hidden text-[#bc2cf0] pt-1.5 pl-2 text-[13px]">
+      <button class="lg:block hidden text-[#bc2cf0] pt-1.5 pl-2 text-[13px] lg:ml-2">
         Zobacz wszystkie
       </button>
 
-      <div class="lg:block hidden border-b lg:ml-2 mt-2" />
+      <div class="lg:block hidden border-b lg:ml-2 mt-2 mr-2 lg:mr-0" />
 
       <div class="lg:block hidden text-[11px] text-gray-500">
-        <div class="pt-4 px-2">© 2023 SnapTale</div>
+        <div class="pt-4 px-2 lg:ml-2">© 2026 SnapTale</div>
       </div>
 
       <div class="pb-14"></div>
